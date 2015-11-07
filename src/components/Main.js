43a2aff6e3a898873302f8792d702173fc09f32a
Christine Cha <hello@christinecha.com>
2015-11-07 16:29:29 -0500

    Added Firebase.

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,26 @@ require('normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import Search from './search';
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
+  componentWillMount() {
+    this.firebaseRef = new Firebase("https://dibbl.firebaseio.com/users");
+    this.firebaseRef.on("child_added", function(snapshot) {
+      var users = [];
+      users.push(snapshot.val());
+      this.setState({
+        users: users,
+      });
+    }.bind(this));
+  };
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
+        <Search />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
---
+++ src/components/search.js
@@ -0,0 +1,18 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class SearchComponent extends React.Component {
+  render() {
+    return (
+      <div className="search">
+        <p>hi</p>
+      </div>
+    );
+  }
+}
+
+export default SearchComponent;
