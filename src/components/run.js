cdb7ca954f076912902e00453c2e2a37cf4ab23c
James O'Dwyer <jx.odwyer@gmail.com>
2015-11-07 17:02:02 -0500

    initial auth :D

--- src/components/run.js
+++ src/components/run.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Main';
+import Router from 'react-router'
+import routes from '../config/routes';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+Router.run(routes, function(Handler){
+  React.render(<Handler /> , document.getElementById('app'));
+});
