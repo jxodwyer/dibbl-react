7c7ab9229c371958b29bec3250f01ea35aa49024
James O'Dwyer <jx.odwyer@gmail.com>
2015-11-08 01:40:41 -0500

    time ticker in room sesh

--- src/components/secure/Room.js
+++ src/components/secure/Room.js
@@ -5,6 +5,17 @@ import React from 'react';
 require('styles/secure/Room.scss');
 
 class RoomComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {ticker: 0}
+    setInterval(this.counter.bind(this), 1000);
+  }
+
+  counter() {
+    var newTicker = this.state.ticker + 1
+    this.setState({ticker: newTicker})
+  }
   componentDidMount() {
     var webrtc = new SimpleWebRTC({
       // the id/element dom element that will hold "our" video
@@ -23,6 +34,7 @@ class RoomComponent extends React.Component {
   render() {
     return (
       <div className="room-component">
+        <h1>{this.state.ticker}</h1>
         <video id="localVideo"></video>
         <div id="remoteVideos"></div>
       </div>
